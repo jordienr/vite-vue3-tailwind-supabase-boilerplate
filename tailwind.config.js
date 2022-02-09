377398895611b64184a3969db55fbe5e401b42b6
Jordi <jordi.err@gmail.com>
2022-02-09 13:44:35 +0100

    upgrade to tailwind@3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   plugins: [],
 }
